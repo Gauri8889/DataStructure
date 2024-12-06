83f972b414b43917462f58849c873e81f660e034
Gauri <dahakegauri4@gmail.com>
2024-12-06 22:59:24 +0530

    validparenthisis

--- namespaceCollection.cpp
+++ namespaceCollection.cpp
@@ -1,1 +1,61 @@
-
+#include<iostream>
+#include<stack>
+using namespace std;
+bool Naaz(string &s, int l)
+{
+  stack<char>st;
+  bool ans=true;
+  for(int i=0; i<l; i++)
+  {
+   if(s[i]=='{'||s[i]=='[' || s[i]=='(')
+            {
+                st.push(s[i]);
+            }
+            else if(s[i]=='}')
+                {
+                if(!st.empty() && st.top()=='{')
+                    {
+                        st.pop();
+                    }
+                    else{
+                        ans=false;
+                        break;
+                    }
+               }
+               else if(s[i]==']')
+                {
+                if(!st.empty() && st.top()=='[')
+                    {
+                        st.pop();
+                    }
+                    else{
+                        ans=false;
+                        break;
+                    }
+               }
+               else if(s[i]==')')
+                {
+                if(!st.empty() && st.top()=='(')
+                    {
+                        st.pop();
+                    }
+                    else{
+                        ans=false;
+                        break;
+                    }
+               }
+  }
+  return ans;
+}
+int main()
+{
+ string s="[}" ;
+ bool i=Naaz(s,s.length()) ;
+ if(i)
+ {
+     cout<<"VALID";
+ }
+ else{
+    cout<<"INVALID";
+ }
+}
--- validParenthesis.cpp
+++ validParenthesis.cpp
@@ -1,4 +1,50 @@
 #include<iostream>
 using namespace std;
 #include<stack>
-bool()
+
+
+bool valid(string &s, int len)
+{
+    stack<char> stk;
+    for (int i = 0; i < len; i++) {
+        if (s[i] == '{' || s[i] == '(' || s[i] == '[') {
+            stk.push(s[i]);
+        }
+        else if (s[i] == '}') {
+            if (!stk.empty() && stk.top() == '{') {
+                stk.pop();
+            } else {
+                return false;  // Invalid closing bracket
+            }
+        }
+        else if (s[i] == ')') {
+            if (!stk.empty() && stk.top() == '(') {
+                stk.pop();
+            } else {
+                return false;  // Invalid closing bracket
+            }
+        }
+        else if (s[i] == ']') {
+            if (!stk.empty() && stk.top() == '[') {
+                stk.pop();
+            } else {
+                return false;  // Invalid closing bracket
+            }
+        }
+    }
+
+    // If stack is empty, it means all brackets are matched
+    return stk.empty();
+}
+
+int main()
+{
+    string s = "[{}]";
+    bool i = valid(s, s.length());
+    if (i) {
+        cout << "valid";
+    } else {
+        cout << "invalid";
+    }
+    return 0;
+}
