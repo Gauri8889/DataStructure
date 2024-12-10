22618dbc0db12405c40fe83ea114a3ef23892965
Gauri <dahakegauri4@gmail.com>
2024-12-10 23:08:02 +0530

    node

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,30 +1,32 @@
-//insertion sort
 #include<iostream>
 using namespace std;
+void insrt(int arr[],int l)
+{
+    for(int i=1;i<l;i++)
+    {
+        int mn=i;
+        while(mn>0 && arr[mn]<arr[mn-1])
+        {
+            int tmp;
+            tmp=arr[mn];
+            arr[mn]=arr[mn-1];
+            arr[mn-1]=tmp;
+            mn--;
+        }
+    }
+    for(int i=0;i<l;i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+}
 int main()
 {
-int arr[]={2,1,7,9,5,4};
-int sorted,tmp;
-int n=sizeof(arr)/sizeof(arr[0]);
-for(int i=0;i<n;i++)
-{
-cout<<arr[i]<<"\t";
-}
-cout<<"\n";
-for(int i=1;i<n;i++)
-{
-    sorted=i;
-
-while(sorted>0 && arr[sorted]<arr[sorted-1])
-{
-tmp=arr[sorted-1];//1
-arr[sorted-1]=arr[sorted];//2
-arr[sorted]=tmp;
-sorted--;
-}
-}
-cout<<"\n after insertion sort\n";
-for(int i=0;i<n;i++)
-{
-cout<<arr[i]<<"\t";
+    int arr[]={8,2,1,5,6};
+    int len=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<len;i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"After applying insertion sort\n";
+    insrt(arr,len);
 }
---
+++ node.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:int data;
+    class node *l;
+    class node *r;
+    public:node(int a)
+    {
+        data=a;
+        l=NULL;
+        r=NULL;
+    }
+};
+int main()
+{
+    node *root=new node(12);
+    root->l=new node(30);
+    root->r=new node(6);
+    cout<<"preorder tree="<<root->data<<"->"<<root->l->data<<"->"<<root->r->data<<"\n";
+    cout<<"inorder tree="<<root->data<<"->"<<root->data<<"->"<<root->r->data<<"\n";
+    cout<<"postorder tree="<<root->data<<"->"<<root->r->data<<"->"<<root->data<<"\n";
+}
